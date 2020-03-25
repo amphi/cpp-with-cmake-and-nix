de162fbb108b21cabc73406e4f2d6d3f3243a6c3
amphi <sebastianeydam@hotmail.com>
2020-03-25 17:57:44 +0100

    Adding a shell.nix file

---
+++ shell.nix
@@ -0,0 +1,16 @@
+/*
+    Nix shell file to import everything needed to build this project.
+    This file is used as a default when running nix-shell.
+*/
+
+{ pkgs ? import <nixpkgs> {} }:
+with pkgs;
+
+stdenv.mkDerivation {
+    name = "build-env";
+    buildInputs = [
+        gcc
+        catch2
+        cmake
+    ];
+}
